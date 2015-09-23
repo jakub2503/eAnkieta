1f2045463a88624df8ca47ca6edffad6764b719c
lukashoo <l.grzeszkowiak@gmail.com>
2015-09-23 13:25:29 +0200

    Revert "User css - mostly done"
    This reverts commit 8352ec94b13d989303c55ca1f5c9ba2dfd7eda0d.

--- app/assets/javascripts/forms.js
+++ app/assets/javascripts/forms.js
@@ -1,40 +1,17 @@
 $(document).ready(function(){
   $(window).load(function(){
      selectorSizes();
-     numberActions();
   });
 });
 
 
 function selectorSizes() {
-	$("#width_tmp").html($('.selector select option:selected').text());
+	$("#width_tmp").html($('select option:selected').text());
     $(".selector").css("width", $("#width_tmp").outerWidth()+33+6+5);
 
     $('.selector select').change(function(){
-    	$("#width_tmp").html($('.selector select option:selected').text());
+    	$("#width_tmp").html($('select option:selected').text());
     	$(".selector").css("width", $("#width_tmp").outerWidth()+33+6+5);
  	});
 }
-
-function numberActions() {
-	$('.plus').click(function(){
-	var temp = parseInt($(this).siblings(":text").val());
-	$(this).siblings(":text").val((temp+1).toString());
- 	});
- 	
- 	$('.minus').click(function(){
-		var temp = parseInt($(this).siblings(":text").val());
-		if ( $(this).parent().hasClass("above_zero") ){
-			if ( temp > 0 ){
-				$(this).siblings(":text").val((temp-1).toString());
-			}
-		}
-		else{
-			if ( (new Date).getFullYear() < temp){
-				$(this).siblings(":text").val((temp-1).toString());
-			}
-		}
- 	});
-}
-
 
